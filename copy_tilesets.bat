9554f147c2c5ac734c70c2d6bb294f82680fefb4
32th-System <intensonat@gmail.com>
2024-02-04 19:06:16 +0100

    Tilesets have to be imported differently from other sprites
    To import tilesets, you'll need to run copy_tilesets.bat or
    copy_tilesets.sh, and then do Scripts > Resource Repackers >
    ImportAllTilesets.csx

---
+++ copy_tilesets.bat
@@ -0,0 +1,4 @@
+@echo off
+mkdir ..\Export_Tilesets
+del /Q ..\Export_Tilesets\*.*
+copy tilesets\*.* ..\Export_Tilesets\
---
+++ copy_tilesets.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir ../Export_Tilesets
+rm -rf ../Export_Tilesets/*
+cp tilesets/* ../Export_Tilesets/
